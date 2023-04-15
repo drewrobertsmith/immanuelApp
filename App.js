f78ef240aa102d5b20abd0926d143479f194fdf3
drewrobertsmith <drew.robert.smith@gmail.com>
2023-04-15 12:26:25 -0500

    Added a Flatlist with 3 columns to simulate FlexWrap

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, FlatList, Image, View } from 'react-native';
+import { SafeArea } from './src/components/safe-area.component';
 
 export default function App() {
+
+  const DATA = [
+    {
+      id: 'asdfhfe8j2f3ojdfwo8er',
+      title: 'Maundy Thursday',
+      description: 'lorum ipsem',
+      artwork: require("./assets/artwork/maundythursday.jpg"),
+    },
+    {
+      id: 'as9dasda98sd',
+      title: 'Good Friday',
+      description: 'lorum ipsem',
+      artwork: require("./assets/artwork/goodfriday.jpg"),
+    },
+    {
+      id: 'lk23j4lk23j4lk234j',
+      title: 'Easter Sunday',
+      description: 'lorum ipsem',
+      artwork: require("./assets/artwork/easter.jpg"),
+    },
+    {
+      id: 'lk23j4lk23423j4lk234j',
+      title: 'Easter Sunday',
+      description: 'lorum ipsem',
+      artwork: require("./assets/artwork/easter.jpg"),
+    },
+    {
+      id: 'lk23j4lk23423j4lk234j',
+      title: 'Easter Sunday',
+      description: 'lorum ipsem',
+      artwork: require("./assets/artwork/easter.jpg"),
+    },
+  ];
+
+
+  const Item = ({ artwork }) => (
+    <Image
+      source={artwork}
+      style={{
+        //flex: 1,
+        width: 143,
+        height: 143,
+
+      }}
+    />
+
+
+  )
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeArea>
+        <View
+          style={{
+            backgroundColor: '#F1F1F1',
+            flex: 1,
+
+          }}
+        >
+          <FlatList
+            data={DATA}
+            renderItem={({ item }) => <Item title={item.title} description={item.description} artwork={item.artwork
+            } />}
+            keyExtractor={item => item.id}
+            numColumns={3}
+            columnWrapperStyle={{
+              backgroundColor: 'red',
+            }}
+          />
+        </View>
+      </SafeArea>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/components/safe-area.component.js
@@ -0,0 +1,7 @@
+import { SafeAreaView, StatusBar, Platform } from "react-native";
+import styled from "styled-components";
+
+export const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
+`;
